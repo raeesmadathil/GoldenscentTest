62d13dc9b7fd87386aebcbcbd003726e9fa98feb
Raees <raeesmadathil@gmail.com>
2019-04-17 20:21:59 +0400

    -created model classes for product and category -added files to corresponding folders

---
+++ GoldenScent Test/Model/Category.swift
@@ -0,0 +1,19 @@
+//
+//  Category.swift
+//  GoldenScent Test
+//
+//  Created by Raees on 17/04/19.
+//  Copyright © 2019 Raees. All rights reserved.
+//
+
+import UIKit
+
+class Category: NSObject {
+
+    var name:String!
+    var icon:UIImage!
+    var banner:UIImage!
+    var besSellers:[[Product]]!
+    var subCategories:[Category]!
+    
+}
---
+++ GoldenScent Test/Model/Product.swift
@@ -0,0 +1,18 @@
+//
+//  Product.swift
+//  GoldenScent Test
+//
+//  Created by Raees on 17/04/19.
+//  Copyright © 2019 Raees. All rights reserved.
+//
+
+import UIKit
+
+class Product: NSObject {
+
+    var name:String!
+    var icon:UIImage!
+    var brand:String!
+    var originalPrice:String!
+    var offerPrice:String!
+}
